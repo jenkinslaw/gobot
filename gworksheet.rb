9cb0a796f69c1b34222727c021a32328feeb37c6
dkinzer <dtkinzer@gmail.com>
2013-06-15 14:28:18 -0400

    Refactor to use module and load XML.

--- gworksheet.rb
+++ gworksheet.rb
@@ -1,33 +1,45 @@
 require 'google/api_client'
+require 'yaml'
 
-# Change client key path if different.
-key = Google::APIClient::KeyUtils.
-  load_from_pkcs12(ENV['HOME'] + '/.ssh/gworksheet.p12', 'notasecret')
+module Jenkins
 
-client = Google::APIClient.new(
-  :application_name => 'Jenkins Google Worksheet',
-  :application_version => '0.1'
-)
-client.authorization = Signet::OAuth2::Client.new(
-  :token_credential_uri => "https://accounts.google.com/o/oauth2/token",
-  :audience => "https://accounts.google.com/o/oauth2/token",
-  :scope => ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/calendar"],
-  :issuer => ENV['GOOGLE_EMAIL'],
-  :signing_key => key
-)
-client.authorization.fetch_access_token!
+  class GoogleApp
+
+    def self::getClient
+      # Change client key path if different.
+      key = Google::APIClient::KeyUtils.
+        load_from_pkcs12(ENV['HOME'] + '/.ssh/gworksheet.p12', 'notasecret')
+
+      client = Google::APIClient.new(
+        :application_name => 'Jenkins Google App',
+        :application_version => '0.1'
+      )
+      client.authorization = Signet::OAuth2::Client.new(
+        :token_credential_uri => "https://accounts.google.com/o/oauth2/token",
+        :audience => "https://accounts.google.com/o/oauth2/token",
+        :scope => [
+          "https://www.googleapis.com/auth/calendar",
+          "https://spreadsheets.google.com/feeds",
+        ],
+        :issuer => ENV['GOOGLE_EMAIL'],
+        :signing_key => key
+      )
+      client.authorization.fetch_access_token!
+      client
+    end
+  end
+
+end
 
 
+client = Jenkins::GoogleApp::getClient()
 drive = client.discovered_api('drive', 'v2')
 calendar = client.discovered_api('calendar', 'v3')
 
 result = client.execute(:api_method => drive.files.get,
                      :parameters => { 'fileId' => ENV['GOOGLE_WORKSHEET_ID']}
                      )
+result = client.execute(:uri => 'https://spreadsheets.google.com/feeds/spreadsheets/private/full')
 
-if (result.status == 200)
-  file = result.data
-  puts "Title: #{file.title}"
-  puts "Description: #{file.description}"
-  puts "MIME type: #{file.mime_type}"
-end
+
+puts YAML::dump(result.body)
